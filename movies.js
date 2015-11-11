40e5e7b679c24af6989eda66b59cde0254b24ade
majumderNirupam <niru.nstu@gmail.com>
2015-11-11 12:25:56 +0600

    understood the realtionship between html file and javascript file and how div and onload function works

---
+++ movies.js
@@ -0,0 +1,10 @@
+function createList(){
+	var s;
+	s = "<ul>"
+		+"<li>Gerilla</li>"
+		+"<li>Bojorongi Vaijan</li>"
+		+"<li>Avenger</li>"
+		+"</ul>";
+	divMovies = document.getElementById("divMovies");
+	divMovies.innerHTML = s;
+}
