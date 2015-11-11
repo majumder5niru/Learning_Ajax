5e4468091f6b6b37b78badbee6a5416eb494f551
majumderNirupam <niru.nstu@gmail.com>
2015-11-11 22:43:53 +0600

    combine the javascript file with html and css dynamically

---
+++ css_in_ajax.js
@@ -0,0 +1,23 @@
+function books(){
+	table = document.getElementById("table");
+	th = document.getElementById("tableHead");
+	item1 = document.getElementById("item1");
+	item2 = document.getElementById("item2");
+
+	table.className = "booksTable";
+	th.className = "booksHead";
+	item1.className = "booksData";
+	item2.className = "booksData";
+}
+
+function writers(){
+	table = document.getElementById("table");
+	th = document.getElementById("tableHead");
+	item1 = document.getElementById("item1");
+	item2 = document.getElementById("item2");
+
+	table.className = "writersTable";
+	th.className = "writersHead";
+	item1.className = "writersData";
+	item2.className = "writersData";
+}
